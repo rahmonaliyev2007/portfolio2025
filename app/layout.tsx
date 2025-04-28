cbca42d807f759e998db9d38562f4f878c43674c
Abdulaziz Raxmonaliyev <rahmonaliyev2007@icloud.com>
2025-04-28 23:59:07 +0500

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#0f0a1c" />
+        {/* <meta name="theme-color" content="#0f0a1c" /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
